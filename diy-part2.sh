c6b6794dbda9b0761ee516ddd429496a5e8872e9
cnzhyu <134799863+cnzhyu@users.noreply.github.com>
2023-06-03 09:07:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,4 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+wget -O target/linux/ipq807x/image/generic.mk https://github.com/hochenchong/Actions-OpenWrt/blob/main/ax6/generic.mk
